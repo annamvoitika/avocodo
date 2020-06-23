59ae9a66078868209d454a06a00da29c89c842ac
kate smith <kateesmith4994@gmail.com>
2020-06-23 10:58:53 +0100

    added 2 routes sign up and in with basic views

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,22 @@
 const HomeController = {
   Index: function(req, res) {
     res.render('home/index', { title: 'Avocodo' });
+  },
+
+  UserSignup: function(req, res) {
+    res.render('home/signup', {});
+  },
+
+  Signup: function(req, res) {
+
+  },
+
+  UserSignin: function(req, res) {
+    res.render('home/signin', {});
+  },
+
+  Signin: function(req, res) {
+
   }
 };
 
--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const HomeController = require('../controllers/home');
 
 router.get('/', HomeController.Index);
+router.get('/signin', HomeController.UserSignin);
+router.post('/signin', HomeController.Signin);
+router.get('/signup', HomeController.UserSignup);
+router.post('/signup', HomeController.Signup);
 
 module.exports = router;
