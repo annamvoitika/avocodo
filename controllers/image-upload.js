fb742f68b4931ddc692335eb84c3b82b16565b1a
Kabir Qazi <kqazi2@ford.com>
2020-07-02 17:45:47 +0100

    Display image on myProfile

--- controllers/image-upload.js
+++ controllers/image-upload.js
@@ -2,26 +2,29 @@ const upload = require('../services/upload');
 const User = require('../models/user')
 
 const ImageUploadController = {
-  Upload: function (req, res, next) {
+  Upload: function (req, res) {
     res.render('image/upload.hbs', {});
   },
 
   Uploaded: function (req, res) {
-    console.log(req);
-    console.log('Kabirs here');
-    console.log('File path before request: ' + req.files.sampleFile.tempFilePath);
+    const sampleFile = req.files.sampleFile
+    const imagePath = '/Users/kqazi2/Projects/week11&12 Final Project/avocodo/public/images/ProfilePics/' + sampleFile.name 
+    const displayImagePath = '/public/images/ProfilePics/' + sampleFile.name 
+    sampleFile.mv(imagePath , function(err) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+    });
     User.findOneAndUpdate({
       _id: req.session.user._id
     },
-      console.log('Name inside update: ' + req.session.user.name), {
-      $set: { name: 'kkca' }
+     {
+      $set: { avatar: displayImagePath }
     },
       function (err, user) {
         if (err) {
           throw err;
         }
-        console.log('Name outside update: ' + req.session.user.name);
-        console.log('File path after request: ' + req.files.sampleFile.tempFilePath);
         res.status(201).redirect('/user/myprofile');
       });
   }
